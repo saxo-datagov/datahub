f70aaa95076df15c783f9ce398355dc12211f504
shubham.garg <shubham.garg@thoughtworks.com>
2020-12-10 15:01:21 +0530

    6800 | add new component for dynamic table render

---
+++ datahub-web/@datahub/shared/addon/components/tables/dynamic-record.ts
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+// @ts-ignore: Ignore import of compiled template
+import template from '../../templates/components/tables/dynamic-record';
+import { layout } from '@ember-decorators/component';
+
+@layout(template)
+export default class TablesDynamicRecord extends Component {}
