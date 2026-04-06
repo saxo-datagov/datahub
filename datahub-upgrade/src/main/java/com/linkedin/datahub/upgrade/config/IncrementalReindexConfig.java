bedfde38902bc8e8ceed9660aae9e359fc844f93
RyanHolstien <RyanHolstien@users.noreply.github.com>
2026-04-06 18:19:57 -0500

    feat(elasticsearch): Elasticsearch side zero-downtime upgrade (#16887)

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/config/IncrementalReindexConfig.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/config/IncrementalReindexConfig.java
@@ -1,10 +1,9 @@
 package com.linkedin.datahub.upgrade.config;
 
 import com.linkedin.datahub.upgrade.conditions.SystemUpdateCondition;
-import com.linkedin.datahub.upgrade.system.BlockingSystemUpgrade;
-import com.linkedin.datahub.upgrade.system.elasticsearch.BuildIndices;
+import com.linkedin.datahub.upgrade.system.NonBlockingSystemUpgrade;
+import com.linkedin.datahub.upgrade.system.elasticsearch.IncrementalReindex;
 import com.linkedin.gms.factory.config.ConfigurationProvider;
-import com.linkedin.gms.factory.search.BaseElasticSearchComponentsFactory;
 import com.linkedin.metadata.entity.AspectDao;
 import com.linkedin.metadata.entity.EntityService;
 import com.linkedin.metadata.graph.GraphService;
@@ -14,23 +13,24 @@ import com.linkedin.metadata.timeseries.TimeseriesAspectService;
 import com.linkedin.metadata.version.GitVersion;
 import io.datahubproject.metadata.context.OperationContext;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
-@Conditional(SystemUpdateCondition.BlockingSystemUpdateCondition.class)
-public class BuildIndicesConfig {
-  @Order(2)
-  @Bean(name = "buildIndices")
-  public BlockingSystemUpgrade buildIndices(
+@Conditional(SystemUpdateCondition.NonBlockingSystemUpdateCondition.class)
+@ConditionalOnProperty(
+    name = "elasticsearch.buildIndices.incrementalReindexEnabled",
+    havingValue = "true")
+public class IncrementalReindexConfig {
+
+  @Bean
+  public NonBlockingSystemUpgrade incrementalReindexNonBlocking(
       final SystemMetadataService systemMetadataService,
       final TimeseriesAspectService timeseriesAspectService,
       final EntitySearchService entitySearchService,
       final GraphService graphService,
-      final BaseElasticSearchComponentsFactory.BaseElasticSearchComponents
-          baseElasticSearchComponents,
       final ConfigurationProvider configurationProvider,
       final AspectDao aspectDao,
       @Qualifier("systemOperationContext") final OperationContext opContext,
@@ -38,17 +38,16 @@ public class BuildIndicesConfig {
       final GitVersion gitVersion,
       @Qualifier("revision") final String revision) {
 
-    return new BuildIndices(
+    String upgradeVersion = String.format("%s-%s", gitVersion.getVersion(), revision);
+    return new IncrementalReindex(
         systemMetadataService,
         timeseriesAspectService,
         entitySearchService,
         graphService,
-        baseElasticSearchComponents,
         configurationProvider,
         aspectDao,
         opContext,
         entityService,
-        gitVersion,
-        revision);
+        upgradeVersion);
   }
 }
